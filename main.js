4d2739b3ba417ef7a9bcd34552807fbcb1e8c932
PonmathiPonraj <ponmathittl@gmail.com>
2024-04-17 20:53:48 -0700

    tribute page

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings: ["A Role Model", "President of India", "Missile Man of India", "Indian Scientist","Padma Bhushan Award", "Padma Vibhushan Award", "The highest civilian honor Bharat Ratna",],
+    typeSpeed:100,
+    backSpeed:80,
+    loop:true
+});
